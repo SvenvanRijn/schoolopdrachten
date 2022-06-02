32c8442797f428919b3acbd02699fac94e3dd36f
Unknown <97091067@st.deltion.nl>
2022-06-02 16:35:23 +0200

    update

--- php/index.php
+++ php/index.php
@@ -2,65 +2,53 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="scripts.js"></script>
+    <title>Pizza's Bestellen</title>
 </head>
 <body>
-    <?php
-        $landErr = "";
-        $naam = "";
-        $nl = $en = $de = $es = $it = $fr = "";
-        if(isset($_POST["submit"])){
-            $naam = $_POST["naam"];
-            $land = $_POST["land"];
-            
-            switch($land){
-                case "NL": 
-                    echo 'Goedemorgen '.$naam;
-                    $nl = "selected";
-                    break;
-                case 'DE': 
-                    echo 'Guten Morgen '.$naam;
-                    $de = "selected";
-                    break;
-                case 'EN':
-                    echo 'Good morning '.$naam;
-                    $en = "selected";
-                    break;
-                case "FR":
-                    echo "Bonjour ".$naam;
-                    $fr = "selected";
-                    break;
-                case "ES":
-                    echo "Ola " .$naam;
-                    $es = "selected";
-                    break;
-                case "IT":
-                    echo "Bien ".$naam;
-                    $it = "selected";
-                    break;
-                default:
-                    $landErr = "U moet een land kiezen";
-            }
-                    
-        } 
+    <?php 
+        $besteldatum = new DateTime("now");
     ?>
-     <form method="post" action="">
-        Naam: <input type="text" name="naam" placeholder="Uw naam" required value="<?php echo $naam ?>"/><br />  
-        Land: <select id="land" name="land" >
-            <option value="" selected>Maak uw keuze</option>
-            <option value="NL" <?php echo $nl ?> >Nederland</option>
-            <option value="DE" <?php echo $de ?>>Duitsland</option>
-            <option value="EN" <?php echo $en ?>>Engeland</option>
-            <option value="FR" <?php echo $fr ?>>Frans</option>
-            <option value="ES" <?php echo $es ?>>Spaans</option>
-            <option value="IT" <?php echo $it ?>>Italiaans</option>
-        </select>
-        <span class="error"><?php echo $landErr ?> </span>
-        <br />
-        <input type="submit" name="submit" value="gegevens versturen" />
+
+    <form action="" method="post">
+        <label for="margherita">Pizza margherita</label> 
+
+        <label for="name">Naam:</label>
+        <br>
+        <input type="text" id="name" name="name" placeholder="Naam" value="" maxlength="100" required>
+        <br>
+        <label for="adres">Adres:</label>
+        <br>
+        <input type="text" id="adres" name="adres" placeholder="Adres" value="" required>
+        <br>
+        <label for="postcode">Postcode:</label>
+        <br>
+        <input type="text" id="postcode" name="postcode" placeholder="1234AB" value="" maxlength="6" pattern="[0-9]{4}[A-Za-z]{2}" required>
+        <br>
+        <label for="plaats">Plaats:</label>
+        <br>
+        <input type="text" id="plaats" name="plaats" placeholder="Plaats" value="" maxlength="100" required>
+        <br>
+        <label for="besteldatum">Besteldatum:</label>
+        <br>
+        <input type="date" id="besteldatum" name="besteldatum" placeholder="Besteldatum" value= "<?php echo $besteldatum->format("Y-m-d"); ?>" readonly required>
+        <br>
+        <label for="bestellen">Bestellen of afhalen?</label>
+        <br>
+        <input type="radio" id="bestellen" name="bestellen" value="Bestellen" required>
+        <label for="bestellen">Bestellen (€5)</label> 
+        <br>
+        <input type="radio" id="afhalen" name="bestellen" value="Afhalen" required>
+        <label for="afhalen">Afhalen</label> 
+        <br>
+        <br>
+        <input type="submit" value="Submit">
     </form>
-    
+    <?php
+        if(isset($_POST["submit"])){
+
+        }
+    ?>
 </body>
 </html>
